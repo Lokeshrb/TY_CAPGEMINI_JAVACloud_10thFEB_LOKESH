894a2372a502ad9ea3efc09cca5b7e7097076bad
Lokesh <lokeshbhavikatte111@gmail.com>
2020-02-27 17:51:17 +0530

    third

--- core-java/src/com/ty/loops/Demo.java
+++ core-java/src/com/ty/loops/Demo.java
@@ -2,6 +2,6 @@ package com.ty.loops;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("This is Demo Class....");
+		System.out.println("This is Demo...");
 	}
 }
